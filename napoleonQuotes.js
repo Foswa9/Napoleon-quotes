179a67719947fef45ad239d125b8468f0eb544d1
Foswa <fcahnevrard@gmail.com>
2023-03-10 23:21:35 +0200

    First

---
+++ napoleonQuotes.js
@@ -0,0 +1,10 @@
+const napoleonQuotes = {
+    1: "My enemies are many, my equals are none.",
+    2: "A general's most important talent is to know the mind of the soldier and gain his confidence... \n He is not a machine that must be made to move, he is reasonable being who needs leadership.",
+    // The quote below is from General Messena
+    3: "His small size and and punny face did not put him in their favour... but as soon as he put on his general's hat, he seemed to grow by two feet. - General Messena",
+    4: "I no longer regarded myself as a simple general, but as a man called upon to decide the fates of peoples."
+}
+
+const napoleonQuotesString = JSON.stringify(napoleonQuotes); 
+console.log(napoleonQuotesString)
