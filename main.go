a34f610a42c3d7daafff8bdc6dc72820b777eb5e
François Evrard <fcahnevrard@gmail.com>
2023-05-04 16:08:21 +0200

    Again

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+// When creating an API you start with the API endpoint. The API endpoint is what the client interacts with.
+// endpoint 1 : /quotes
+// GET- Get the list of ALL quotes in the form of JSON.
+// POST - Add a quote in the form of JSON
+// endpoint 2 : /quotes/:id
+// GET - get a specific quote specified by the ID
 type napoleonQuote struct {
 	ID     string `json:"id"`
 	Quote  string `json:"quote"`
 	Author string `json:"author"`
 }
 
+// As of right now the data is stored in memory, though usually this is stored in a database.
+// Because its stored in memory, each time the server is closed, data cannot be retrieved. Only when server is running can the data be retrieved.
 var quotes = []napoleonQuote{
 
 	{ID: "1", Quote: "My enemies are many, my equals are none.", Author: "Napoleon"},
@@ -20,11 +35,49 @@ var quotes = []napoleonQuote{
 	{ID: "4", Quote: "I no longer regarded myself as a simple general, but as a man called upon to decide the fates of peoples", Author: "Napoleon"},
 }
 
+// Now a handler needs to be created.
+// When the client makes a request at GET /quotes, all the quotes must be returned as JSON.
+// To do this you need 1.Logic to prepare the response 2. Code to map the request path to your logic.
+
 func getQuotes(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, quotes)
 }
 
+func Init() *gorm.DB {
+	db, err := gorm.Open(postgres.Open("DATABASE_URL"), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
+
 func main() {
+	db, err := gorm.Open(postgres.Open("DATABASE_URL"), &gorm.Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "You cannot connect to the database", err)
+	}
+
+	database, _ := db.DB()
+	err = database.Ping()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "The ping did not work my boy", err)
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATBASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "My brother, you are unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: &V\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(greeting)
+
 	router := gin.Default()
 	router.GET("/quotes", getQuotes)
 	router.Run("localhost:9000")
